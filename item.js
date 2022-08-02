530c8a3393d3a2708ef84ad1c4ff86b12c4cf636
aref <arefobaid6@gmail.com>
2022-08-02 22:00:06 +0200

    protfolio

---
+++ item.js
@@ -0,0 +1,13 @@
+function chengeThemaAndInfo(){
+  var item=document.querySelectorAll("#thema li");
+      var tab=[];
+
+       for(var i=0;i<item.length;i++){
+        tab.push(item[i].innerHTML);
+       }
+
+       console.log(item.indexof("tehma1"));
+
+
+}
+
